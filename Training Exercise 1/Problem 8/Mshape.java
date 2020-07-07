e16b04778f946dd306fd13858c1ca98d288c9226
amarj132 <67458140+amarj132@users.noreply.github.com>
2020-07-07 16:50:57 +0530

    Create Mshape.java

---
+++ Training Exercise 1/Problem 8/Mshape.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class mshape{
+		public static void main(String s[]){
+			Scanner sc=new Scanner(System.in);
+			String str="ABCDEF";
+			int len=str.length();
+			int space,k=len-2;
+			for(int i=0;i<len;i++)
+			{
+				space=2*i-1;
+				System.out.print(str.substring(0,len-i));
+				for(int j=1;j<=space;j++)
+					System.out.print(" ");
+				for(;k>=0;k--){
+					System.out.print(str.charAt(k));
+				}
+				k=len-i-2;
+				System.out.println();
+				
+				
+			}
+		}
+}
