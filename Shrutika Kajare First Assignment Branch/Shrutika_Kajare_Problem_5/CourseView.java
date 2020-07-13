ae7059a955a4c6e8f93649b73c05407b492defc8
shrutika2905 <67473440+shrutika2905@users.noreply.github.com>
2020-07-13 17:31:09 +0530

    Create CourseView.java

---
+++ Shrutika Kajare First Assignment Branch/Shrutika_Kajare_Problem_5/CourseView.java
@@ -0,0 +1,10 @@
+class CourseView
+{
+	public void printCourseDetails(String CourseName, String CourseID, String CourseCategory)
+	{
+		System.out.println("Course Details");
+		System.out.println("Course Name: "+CourseName);
+		System.out.println("Course ID: "+CourseID);
+		System.out.println("Course Category: "+CourseCategory);
+	}
+}
